9e38e2aa82b1c56531e8b5a939e97078c21634cc
王思函 <wangsihan@wangsihandeMacBook-Air.local>
2026-05-14 16:16:02 +0900

    fix font issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next"
 import "./globals.css"
 import { ThemeProvider } from "@/components/ThemeProvider"
-import { Geist } from "next/font/google";
+import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "PDF ノートメーカー",
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="ja" suppressHydrationWarning className={cn("font-sans", geist.variable)}>
+    <html lang="ja" suppressHydrationWarning className={cn("font-sans", inter.variable)}>
       <body>
         <ThemeProvider
           attribute="class"
